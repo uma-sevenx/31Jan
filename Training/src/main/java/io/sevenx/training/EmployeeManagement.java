215fafc37ee6f1fe96434481641ef7c54ffcb768
Umashankar Arora <uma@sevenx.io>
2022-02-02 08:12:09 +0530

    Initial Commit

---
+++ Training/src/main/java/io/sevenx/training/EmployeeManagement.java
@@ -0,0 +1,13 @@
+package io.sevenx.training;
+
+public class EmployeeManagement {
+	
+	public static void main(String[] args) {
+		System.out.println("Test");
+	}
+	
+	public static String reverse(String input) {
+		return input;
+	}
+
+}
---
+++ Training/src/test/java/io/sevenx/training/EmployeeManagementTest.java
@@ -0,0 +1,15 @@
+package io.sevenx.training;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EmployeeManagementTest {
+
+	@Test
+	void test() {
+		//fail("Not yet implemented");
+		assertTrue(true);
+	}
+
+}
